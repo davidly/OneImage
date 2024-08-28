201694f2ab415aa69e27888382c620117930b8dd
davidly <davidly_lee@hotmail.com>
2024-08-28 12:51:15 -0700

    link static so risc-v binaries can run in rvos

--- mar.sh
+++ mar.sh
@@ -1,2 +1,2 @@
 # turn off deprecated warnings since this code also needs to build on CP/M 2.2
-g++ -Wno-deprecated -ggdb -Ofast -fno-builtin -D FORCETRACING -D NDEBUG -I . oia.c oidis.c -o oia
+g++ -Wno-deprecated -ggdb -Ofast -fno-builtin -D FORCETRACING -D NDEBUG -I . oia.c oidis.c -o oia -static
